f6be6f32ecaf7f8aae83b3edf8282daa145a076d
lucasdaher <contato@lucasdaher.com>
2024-02-26 22:13:21 -0300

    style: add box-shadow to cards.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -15,7 +15,7 @@ const Card = ({
       {cardSize === "large" ? (
         <div
           className="flex flex-col justify-start items-center p-card-small lg:p-card-large w-card h-card-small lg:h-card-large bg-gradient-to-br from-blue-purple-start to-blue-purple-end
-       rounded-card-large z-20 transition-all duration-200 lg:last:mb-0 last:mb-14"
+       rounded-card-large z-20 shadow-[0px 20px 40px 0px rgba(212, 210, 244, 0.50)] transition-all duration-200 lg:last:mb-0 last:mb-14"
         >
           <div className="title text-white text-lg font-bold">{cardTitle}</div>
 
@@ -38,7 +38,7 @@ const Card = ({
           <LearnMoreButton cardSize={"large"} />
         </div>
       ) : (
-        <div className="flex flex-col justify-center items-center p-card-small w-card h-card-small bg-white rounded-card-small z-0 transition-all duration-200 lg:last:mb-0 last:mb-24">
+        <div className="flex flex-col justify-center items-center p-card-small w-card h-card-small bg-white shadow-[0px 20px 40px 0px rgba(212, 210, 244, 0.50)] rounded-card-small z-0 transition-all duration-200 lg:last:mb-0 last:mb-24">
           <div className="title text-gray text-lg font-bold">{cardTitle}</div>
 
           <div className="price-container flex justify-center items-center gap-2 mb-8 mt-6">
